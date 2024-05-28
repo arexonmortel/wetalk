92d5aa40b96f2b3e488e5ffc34b71f41381d4acf
Arexon Mortel <96506522+arexonmortel@users.noreply.github.com>
2024-05-28 09:16:39 +0800

    Setup the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import Login from "./components/Login"
+import {Routes, Route} from 'react-router-dom'
+import Register from "./components/Register"
 
 function App() {
   
   return (
-  <div className="font-sans">
-    Hello World
+  <div className="p-10">
+    <Routes>
+      <Route path = '/' element = {<Login/>} />
+      <Route path = '/register' element = {<Register/>} />
+    </Routes>
   </div>
   )
 }
