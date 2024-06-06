22cb6db944a5feec19b62b371f0115d3a7f3ee2e
Arexon Mortel <96506522+arexonmortel@users.noreply.github.com>
2024-06-06 08:38:26 +0800

    created backend

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Get the MongoDB URI from environment variables
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.log('Error connecting to MongoDB');
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
